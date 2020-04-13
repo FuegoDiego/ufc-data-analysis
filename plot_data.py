36d818fa38ee6fc009a014d463c0b22fa96e50c8
Diego <diego.caballero14@gmail.com>
2020-04-13 17:15:57 -0400

    The heat map now uses a logarithmic scale for the colours of the plot

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import warnings
 import pdb
 
@@ -24,20 +25,23 @@ def plot_heat_strikes(df, stk_type, width=19.20, height=10.8, fmt='.png',
     # rows_exc: lst of str, weight classes to exclude from the plot
     # switch_ax: Boolean, True if you want to switch the x and y axes on the plot
     
+    p1 = 'Average Strikes '
+    p2 = 'per 15 Minutes For Each Weight Class, by Strike Type'
+    
     # define flag to filter columns by landed or attempted strikes
     if stk_type.upper() == 'LANDED':
         t = '_L'
         fname = './output/strikes-per-wc-landed' + fmt
-        plt_title = 'Average Strikes ' + 'Landed ' + 'For Each Weight Class, by Strike Type'
+        plt_title = p1 + 'Landed ' + p2
     elif stk_type.upper() == 'ATTEMPTED':
         t = '_A'
         fname = './output/strikes-per-wc-attempted' + fmt
-        plt_title = 'Average Strikes ' + 'Attempted ' + 'For Each Weight Class, by Strike Type'
+        plt_title = p1 + 'Attempted ' + p2
     else:
         warnings.warn('Invalid strike type provided. Defaulting to "landed"')
         t = '_L'
         fname = './output/strikes-per-wc-landed' + fmt
-        plt_title = 'Average Strikes ' + 'Landed ' + 'For Each Weight Class, by Strike Type'
+        plt_title = p1 + 'Landed ' + p2
 
     # only keep the columns of the provided strike type
     df_heat = df.loc[:, list(map(lambda s: t in s, list(df.columns)))].copy()
@@ -59,8 +63,10 @@ def plot_heat_strikes(df, stk_type, width=19.20, height=10.8, fmt='.png',
     sns.set(rc={'figure.figsize':(width,height)})
     sns.set(font_scale=1.5)
     
-    sns_plot = sns.heatmap(data=df_heat, annot=True, fmt='g')
-    plt.title(plt_title)
+    vmin = df_heat.values.min()
+    vmax = df_heat.values.max()
+    sns_plot = sns.heatmap(data=df_heat, annot=True, fmt='g', norm=LogNorm(vmin=vmin, vmax=vmax))
+    plt.title(plt_title, fontsize=22)
     plt.yticks(rotation=0)
     
     # tighten the plot so it fits better
