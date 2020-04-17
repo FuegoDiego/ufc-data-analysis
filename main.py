78dd7c268a1286c0f0ec365b59f676aff3b7238f
Diego <diego.caballero14@gmail.com>
2020-04-17 17:19:29 -0400

    Cleaned up variable and function names. Improved comments

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from clean_data import *
 from calculate_metrics import *
 
 filepath_fights = './input/raw_total_fight_data.csv'
-df_fights_raw = pd.read_csv(filepath_fights, sep=';', index_col=False)
+FIGHTS_RAW = pd.read_csv(filepath_fights, sep=';', index_col=False)
 
 filepath_fighters = './input/raw_fighter_details.csv'
-df_fighters = pd.read_csv(filepath_fighters, index_col='fighter_name')
+FIGHTERS = pd.read_csv(filepath_fighters, index_col='fighter_name')
 
 # list of weight classes ordered from lowest weight to largest weight, with
 # women divisions first. Catch, Open, and undefined weights go last
@@ -27,22 +27,22 @@ acc_cols = {'BODY', 'CLINCH', 'DISTANCE', 'GROUND', 'HEAD', 'LEG', 'SIG_STR.', '
 pct_cols = {'SIG_STR_pct', 'TD_pct'}
 
 # copy the raw data and clean it
-df_fights = df_fights_raw.copy()
-replace_acc_stat(df_fights, acc_cols)
-replace_pct_stat(df_fights, pct_cols)
-df_fights['weight_class'] = df_fights['Fight_type'].apply(get_weight_class)
-df_fights = get_fight_time(df_fights)
+FIGHTS = FIGHTS_RAW.copy()
+ReplaceAccStat(FIGHTS, acc_cols)
+ReplacePctStat(FIGHTS, pct_cols)
+FIGHTS['weight_class'] = FIGHTS['Fight_type'].apply(GetWeightClass)
+FIGHTS = CalcFightTime(FIGHTS)
 
 # get the average of all statistics for every fighter, throughout their career
-fights_avg = calc_avg_fight(df_fights, acc_cols)
+FIGHT_AVG = CalcAvgFight(FIGHTS, acc_cols)
 
 # get a DataFrame with the fighter orig names, new names with weight class, and
 # weight class
-weight_class = create_weight_class(pd.DataFrame(fights_avg.index))
+WEIGHT_CLASS = GetClassFromFighter(pd.DataFrame(FIGHT_AVG.index))
 
 # calculate average for each accuracy column
-weight_class_avg = calc_avg_per_class(fights_avg, weight_class, acc_cols)
-weight_class_avg = weight_class_avg.reindex(weight_class_sort)
+WEIGHT_CLASS_AVG = CalcAvgPerClass(FIGHT_AVG, WEIGHT_CLASS, acc_cols)
+WEIGHT_CLASS_AVG = WEIGHT_CLASS_AVG.reindex(weight_class_sort)
 
 # get the total number of fights, per fighter
-fight_count = get_n_fights(df_fights)
+FIGHT_COUNT = CalcNumFights(FIGHTS)
